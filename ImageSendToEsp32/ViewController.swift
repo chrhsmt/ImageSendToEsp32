1ba658eff12f478c4d3810fcb07734a67111a8b4
chihiro hashimoto <chr@chrhsmt.com>
2017-04-18 00:26:52 +0900

    Initial Commit

--- ImageSendToEsp32/ViewController.swift
+++ ImageSendToEsp32/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func sendToEsp32(_ sender: UIButton) {
+        NSLog("sendToEsp32")
+        let url = "http://192.168.1.101"
+        let request: NSMutableURLRequest = AFHTTPRequestSerializer().multipartFormRequest(withMethod: "POST", urlString: url, parameters: nil, constructingBodyWith: { (data: AFMultipartFormData) in
+                let imageData:Data = UIImagePNGRepresentation(#imageLiteral(resourceName: "Image"))!
+                data.appendPart(withFileData: imageData, name: "file", fileName: "file.png", mimeType: "image/png")
+            }, error: nil)
+        let manager:AFHTTPSessionManager = AFHTTPSessionManager(sessionConfiguration: URLSessionConfiguration.default)
+        let task:URLSessionUploadTask = manager.uploadTask(withStreamedRequest: request as URLRequest, progress: { (progress: Progress) in
+            NSLog("Progress: %d", progress.fractionCompleted)
+        }) { (response: URLResponse, id: Any?, error: Error?) in
+            if ((error) != nil) {
+                NSLog(error!.localizedDescription)
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ImageSendToEsp32' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ImageSendToEsp32
+  pod 'AFNetworking', '~> 3.0'
+
+  target 'ImageSendToEsp32Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ImageSendToEsp32UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
