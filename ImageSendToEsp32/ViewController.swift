291a22c852481e8baa4e1c95cde7016b224a04b8
chihiro hashimoto <chr@chrhsmt.com>
2017-04-17 18:05:41 +0900

    Initial Commit

---
+++ ImageSendToEsp32/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSendToEsp32
+//
+//  Created by chihiro hashimoto on 2017/04/17.
+//  Copyright © 2017年 chihiro hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
